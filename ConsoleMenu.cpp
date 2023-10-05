818abd21047a4da439ca31b13569e993027c8bbc
NISSAN-180SX-NISMO <semenaksin13@gmail.com>
2023-10-05 07:48:15 +0300

    update 2.0 big patch new LOCATION and beautiful OST for my game subscribe and follow me pls

--- ConsoleMenu.cpp
+++ ConsoleMenu.cpp
@@ -1,4 +1,4 @@
-#include "ConsoleMenu.h"
+#include "../../../Headers/ConsoleMenu/ConsoleMenu.h"
 
 Menu::FunctionCase::FunctionCase(const string title, KEY key, function<void()> ExeFunc)
 {
@@ -24,29 +24,35 @@ Menu::Menu() {
 }
 
 void Menu::push_case(string title, KEY key, function<void()> ExeFunc) {
-	if (menu[char(key)]) throw DublicateKey("Дубликат ключа!!!", char(key));
+	if (menu[char(key)]) throw DuplicateKey("Duplicate Key!!!", char(key));
 	menu[char(key)] = new FunctionCase(title, key, ExeFunc);
 }
 
 void Menu::push_case(string title, KEY key, Menu* SubMenu) {
-	if (menu[char(key)]) throw DublicateKey("Дубликат ключа!!!", char(key));
+	if (menu[char(key)]) throw DuplicateKey("Duplicate Key!!!", char(key));
 	menu[char(key)] = new SubMenuCase(title, key, SubMenu);
 }
 
 void Menu::start(Menu* menu) {
 	while (true) {
 		system("cls");
-		std::cout << "Esc) Выход/назад" << std::endl;
-		for (int i = 0; i < 256; ++i) {
-			if (menu->menu[i])
+		std::cout << "Esc) BACK/EXIT" << std::endl;
+		for (int i = 0; i < 256; ++i)
+			if (menu->menu[i] && i != 48)
 				std::cout << menu->menu[i]->title << std::endl;
-		}
+		if (menu->menu[48])
+			std::cout << menu->menu[48]->title << std::endl;
 		char choice = _getch();
+		std::cout << std::endl;
 		if (choice == char(KEY::Esc))
 			break;
-		if (!menu->menu[choice])
-			throw CaseIsMissing("Такого пункта нет в меню!");
-		auto SubMenu = menu->menu[choice]->Do();
-		if (SubMenu) start(SubMenu);
+		if (!menu->menu[choice]) {
+			std::cout << "Unknown key!!! Press something for continue...";
+			_getch();
+		}
+		else {
+			auto SubMenu = menu->menu[choice]->Do();
+			if (SubMenu) start(SubMenu);
+		}
 	}
 }
